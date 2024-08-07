da3c9794151f264ef8e59faf7e7ae79f077863b7
natsmartin <natsmartin95@gmail.com>
2024-08-07 16:28:51 +0800

    refactor term, location, styling

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -6,26 +6,40 @@ import '../styles/Search.css';
 function Search() {
 
   const dispatch = useDispatch()
-  const [filter, setFilter] = useState('best_match')
+  const [filter, setFilter] = useState('')
+  const [term, setTerm] = useState('')
+  const [location, setLocation] = useState('')
 
   const onClickSort = (sortby) => {
-    setFilter(sortby)
-    onClick(sortby)
+    const termInput = document.getElementById('search-term').value
+    const locationInput = document.getElementById('search-location').value
+    setTerm(termInput)
+    setLocation(locationInput)
+    setFilter(sortby.value)
+  }
+
+  const active = {
+    'color': '#ffc23e',
+    'border-bottom': '1px solid #ffc23e'
+  }
+
+  const inActive = {
+    'color': 'white',
+    'border-bottom': '1px solid white'
   }
 
 
   const onClick = (filter) => {
-    const term = document.getElementById('search-term').value
-    const location = document.getElementById('search-location').value
     dispatch(fetchBusinesses({ term: term, location: location, sortby: filter }))
+    console.log(`Searching Yelp with ${term}, ${location}, ${filter}`)
   }
 
   return (
     <div className='search-container'>
       <div className='sort-container'>
-        <button className='sort-button' id='btn-best-match' value='best_match' onClick={() => onClickSort('best_match')}>Best Match</button>
-        <button className='sort-button' id='btn-highest-rated' value='rating' onClick={() => onClickSort('rating')}>Highest Rated</button>
-        <button className='sort-button' id='btn-most-reviewed' value='review_count' onClick={() => onClickSort('review_count')}>Most Reviewed</button>
+        <button className='sort-button' style={filter === 'best_match' ? active : inActive} id='btn-best-match' value='best_match' onClick={(e) => onClickSort(e.target)}>Best Match</button>
+        <button className='sort-button' style={filter === 'rating' ? active : inActive} id='btn-highest-rated' value='rating' onClick={(e) => onClickSort(e.target)}>Highest Rated</button>
+        <button className='sort-button' style={filter === 'review_count' ? active : inActive} id='btn-most-reviewed' value='review_count' onClick={(e) => onClickSort(e.target)}>Most Reviewed</button>
       </div>
       <div className='input-container'>
         <input className='search-input' id="search-term" type='search' placeholder='Search' />
